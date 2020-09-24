6247927ff3a02b19a7365a3e71df4c14194ebf98
Jackson Sippe <jackson123198@gmail.com>
2020-09-24 01:00:42 -0400

    Moved database columns around and made number of visits default to 1

--- program.py
+++ program.py
@@ -1,20 +1,33 @@
 from tkinter import *
-
+from tkinter import messagebox
 from PIL import ImageTk,Image
 import sqlite3
+import re
 
 root = Tk()
 root.title('Chocolate Cellar Customer Loyalty')
 root.geometry("400x400")
 
-conn = sqlite3.connect('database.db')
-
-c = conn.cursor()
-
 def submit():
-    if f_name == '':
-        print('test')
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    name_rex = re.compile("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")
+    phone_rex = re.compile(
+    if not name_rex.match(f_name.get()):
+        messagebox.showerror("showerror","Invalid first name")
+        return
+    if not name_rex.match(l_name.get()):
+        messagebox.showerror("showerror","Invalid last name")
+        return
 
+    c.execute("INSERT INTO customers (first_name, last_name, email_address, phone_number) VALUES (?, ?, ?, ?)", (f_name.get(), l_name.get(), email_address.get(), phone_number.get()))
+    conn.commit()
+    conn.close()
+    f_name.delete(0, END)
+    l_name.delete(0, END)
+    phone_number.delete(0, END)
+    email_address.delete(0, END)
+    
 # Create text boxes
 f_name = Entry(root, width=30)
 f_name.grid(row=0, column=1, padx=20)
@@ -39,6 +52,4 @@ email_address_label.grid(row=3, column=0)
 submit_button = Button(root, text="Add Customer", command=submit)
 submit_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
-conn.commit()
-conn.close()
 root.mainloop()
--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers (
-	phone_number TEXT PRIMARY KEY,
-	joined TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	first_name TEXT,
 	last_name TEXT,
 	email_address TEXT,
-	num_visits INTEGER NOT NULL
+	phone_number TEXT PRIMARY KEY,
+	joined TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	num_visits INTEGER NOT NULL DEFAULT 1
 );
